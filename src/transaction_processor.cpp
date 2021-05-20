130c6e3567f3a98de722e41c9d4f0c73405af417
Carlos Alejandro Gutiérrez Sandoval <carlos.gutierrez@gluwa.com>
2021-05-20 17:47:32 -0500

    follow the original equilvalent impl

--- src/transaction_processor.cpp
+++ src/transaction_processor.cpp
@@ -47,14 +47,12 @@ static void s_signal_handler (int signal_value) {
 
 static void s_catch_signals (void) {
     if (!s_interrupt_initialized) {
-        //struct sigaction action;
-        //action.sa_handler = s_signal_handler;
-        //action.sa_flags = 0;
-        //sigemptyset (&action.sa_mask);
-        //sigaction (SIGINT, &action, NULL);
-        //sigaction (SIGTERM, &action, NULL);
-        signal(SIGINT, &s_signal_handler);
-        signal(SIGTERM, &s_signal_handler);
+        struct sigaction action;
+        action.sa_handler = s_signal_handler;
+        action.sa_flags = 0;
+        sigemptyset (&action.sa_mask);
+        sigaction (SIGINT, &action, NULL);
+        sigaction (SIGTERM, &action, NULL);
         s_interrupt_initialized = true;
 
         LOG4CXX_ERROR(logger, "signal handler initialized");
